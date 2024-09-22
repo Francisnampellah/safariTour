5e20fa3c86f3234102812a245ab965e9e5acbab4
francisnampellah <bnampellah1@gmail.com>
2024-09-22 16:08:55 +0300

    the

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -5,6 +5,8 @@ import { FaConciergeBell } from "react-icons/fa";
 import { BsFillPatchQuestionFill } from "react-icons/bs";
 import { MdGroups } from "react-icons/md";
 import { FaLocationDot } from "react-icons/fa6";
+import { IoMdArrowRoundForward } from "react-icons/io";
+
 
 
 import AccordionItem from "~/component/Accordion";
@@ -158,6 +160,7 @@ const candidates: Candidate[] = [
 ];
 
 
+import { IoMdArrowRoundForward } from "react-icons/io";
 
 
 export default function Index() {
@@ -222,6 +225,7 @@ export default function Index() {
         </div>
 
         <div className="px-6">
+          <PopularPackage />
           <AboutUsSection />
           <ServicesSection />
           <Brands />
@@ -269,6 +273,47 @@ export default function Index() {
 
 /// MAIN COMPONENTS 
 
+
+
+const PopularPackage = () => (
+  <section
+    id="section-1"
+    className="flex flex-col justify-start py-20 md:py-40 gap-4 md:gap-8 h-screen px-6 md:px-16 scrollSnap"
+  >
+
+    <div className="flex justify-between">
+
+      <h2 className="text-3xl md:text-4xl font-semibold text-start text-blue-500 mb-6 md:mb-8 flex gap-4 md:gap-8 items-center">
+        <FcAbout className="h-12 md:h-16 w-12 md:w-16" /> POPULAR PACKAGES
+      </h2>
+      <IoMdArrowRoundForward className="w-8 h-8 cursor-pointer" />
+    </div>
+
+    <div>
+      <p className="text-2xl text-gray-600">
+        We are a passionate team of travel enthusiasts dedicated to making your travel dreams come true.
+      </p>
+    </div>
+
+    <div className="flex flex-col md:flex-row w-full gap-4">
+
+      {[1, 2, 3, 4].map((item) => (
+        <div key={item} className="flex flex-1 flex-col items-center h-[60vh] bg-contain rounded-3xl overflow-hidden m-2" style={{ backgroundImage: `url(${AllImages.BannerImages.imageUrl})` }}>
+          <div className="w-full h-full gap-2 bg-black flex flex-col justify-end items-start bg-opacity-40 px-4 md:px-8 py-4">
+            <div className="border border-white rounded-full px-4 py-2 text-white">
+              Explore Here
+            </div>
+            <div className="flex flex-col bottom-4 left-4 text-white w-full">
+              <h3 className="text-lg md:text-xl font-semibold">Expert Travel Advice</h3>
+              <p className="text-xs md:text-sm mt-1">Tips and guides to enhance your travel experience.</p>
+            </div>
+          </div>
+        </div>
+      ))}
+
+    </div>
+  </section>
+);
 
 
 
